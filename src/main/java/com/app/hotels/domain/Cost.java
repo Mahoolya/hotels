00d8fcf01c8a339eb9c73b742df1296c747e6eba
Mahoolya <marybokhan35@gmail.com>
2023-03-20 16:09:05 +0300

    Testing of booking service was added

--- src/main/java/com/app/hotels/domain/Cost.java
+++ src/main/java/com/app/hotels/domain/Cost.java
@@ -1,7 +1,9 @@
 package com.app.hotels.domain;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -10,6 +12,8 @@ import java.util.List;
 @Entity
 @Table(name = "costs")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Cost {
 
     @Id
